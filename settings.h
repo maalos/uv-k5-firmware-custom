3a742599bff63bc9833349fea63a9bb2c4ef24ef
maalos <54448857+maalos@users.noreply.github.com>
2024-08-26 00:25:28 +0200

    Update settings.h

--- settings.h
+++ settings.h
@@ -113,7 +113,8 @@ typedef enum ALARM_Mode_t ALARM_Mode_t;
 enum ROGER_Mode_t {
 	ROGER_MODE_OFF = 0,
 	ROGER_MODE_ROGER,
-	ROGER_MODE_MDC
+	ROGER_MODE_MDC,
+	ROGER_MODE_AIRPORT
 };
 typedef enum ROGER_Mode_t ROGER_Mode_t;
 
